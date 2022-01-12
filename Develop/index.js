94aa2b02c79846572af5e3fe3ed6b26dec22e5c2
Maryia <malakhava@yahoo.com>
2022-01-11 20:14:12 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
-// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username? (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                  return true;
+                } else {
+                  console.log('Please enter your GitHub username!');
+                  return false;
+                }
+              }
+            },
+          {
+            type: "input",
+            name: "e-mail",
+            message: "What is your e-mail?",
+            },
+          {
+            type: "input",
+            name: "project_name",
+            message: "What is the name of your project?", 
+            },
+          {
+            type: "input",
+            name: "description",
+            message: "Please, describe your project",
+            },
+          {
+            type: "input",
+            name: "installation",
+            message: "Please, tell about installation requierements",
+            },
+          {
+            type: "input",
+            name: "usage",
+            message: "Please, provide instructions for use",
+           },
+          {
+            type: "list",
+            name: "license",
+            message: "Please, choose the license",
+            choices: ["Permissive", "Public", "Proprietary", "Copyleft", "LGPL"],
+           },
+          {
+            type: "input",
+            name: "collaborators",
+            message: "Please, put names of your collaborators",
+            },
+          {
+            type: "input",
+            name: "test",
+            message: "Please, provide tests and instructions to testing",
+          },
+          {
+            type: "input",
+            name: "questions",
+            message: "Please, list frequently asked questions and your reach out information",
+          }
+    ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
